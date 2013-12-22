d43b8824fa6c01811fe2e9cc4fa19c4778d488a7
Mark Moissette <kaosat.dev@gmail.com>
2013-12-22 16:48:50 +0100

    - lots of updates and improvement to the socket-io element  * fixed / cleaned up all socket.io event handles (connect, disconnect, error, message etc)  * added support for listening to specific events (listenTo attribute)  * auto connection is now optional and settable via autoConnect attribute  * outgoing event name can now be set via outEventName  * seperated incomming and outgoing message to two seperate attributes (for easier databinding)
    - updated demo server
    - updated example in index.html with usage within another custom element

--- demo-server.js
+++ demo-server.js
@@ -1,8 +1,29 @@
-var io = require('socket.io').listen(8081);
+var io = require('socket.io').listen(8082);
+io.set('log level', 1);
+
+
+clients = []
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
+
+  clients.push(socket.id);
+  console.log("Client " + socket.id +" connected");
   socket.on('my other event', function (data) {
     console.log(data);
   });
+  socket.on('message', function (data) {
+    console.log("SERVER recieved message",data);
+  });
+  socket.on('foo', function (data) {
+    console.log("SERVER recieved foo",data);
+  });
+
+  var index = 0;
+  function spamMessage()
+  {
+    socket.emit('news', { hello: 'world'+index });
+    index +=1;
+  }
+
+  setInterval( spamMessage, 700);
 });
